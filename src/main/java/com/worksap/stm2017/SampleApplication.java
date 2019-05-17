69dcd2d0a7a5c5568f3988b1c052cf2b4ce88c52
xu_xi <xu_xi@worksap.co.jp>
2019-05-17 16:50:48 +0800

    change applications.properties to fit docker

--- src/main/java/com/worksap/stm2017/SampleApplication.java
+++ src/main/java/com/worksap/stm2017/SampleApplication.java
@@ -1,5 +1,7 @@
 package com.worksap.stm2017;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,9 +15,48 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApplication {
     public static void main(String[] args) {
+    	System.out.println("http://127.0.0.1:8083/(paiban/)index");
+    	System.out.println("需要开启es，MQ，zookeeper,kafka");
     	System.out.println(StringUtils.isEmpty(""));
     	System.out.println(StringUtils.isEmpty(" "));
     	System.out.println(StringUtils.isEmpty(null));
+    	String qry11="select e.emp_id,emp_no,emp_nm,sr_syz_nm from srw_syz_manager sm,srw_syozoku sz,emp e,srw_syainmst sy,srw_jigyosyo ji where  sm.sya_id = e.emp_id AND sm.sdate <=  to_date('2019/04/22','YYYY/MM/DD')  AND sm.edate >=  to_date('2019/04/22','YYYY/MM/DD')  AND sm.sr_syz_cd = sz.sr_syz_cd  AND sz.sdate <=  to_date('2019/04/22','YYYY/MM/DD')  AND sz.edate >=  to_date('2019/04/22','YYYY/MM/DD')  AND sz.status = 0  AND sm.sya_id = sy.sya_id AND sy.boss_kb = 0 AND sy.status = 0 AND sy.sdate <=  to_date('2019/04/22','YYYY/MM/DD')  AND sy.edate >=  to_date('2019/04/22','YYYY/MM/DD')  and ji.sdate <=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and ji.edate >=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and sz.sr_jig_cd = ji.sr_jig_cd and ji.sr_tkt_cd = 'DFLT' UNION SELECT e.emp_id,e.emp_no,e.emp_nm,sr_syz_nm  FROM srw_syz_submanager ss,srw_syozoku sz,emp e,srw_syainmst sy,srw_jigyosyo ji WHERE serviceName = 'root.cws.shuro.boss.time_input_approval' AND ss.sya_id = e.emp_id AND ss.sr_syz_cd = sz.sr_syz_cd  AND sz.sdate <=  to_date('2019/04/22','YYYY/MM/DD')  AND sz.edate >=  to_date('2019/04/22','YYYY/MM/DD')  AND ss.sdate <=  to_date('2019/04/22','YYYY/MM/DD')  AND ss.edate >=  to_date('2019/04/22','YYYY/MM/DD')  AND sz.status = 0  AND sy.sya_id = ss.sya_id AND sy.boss_kb = 0 AND sy.status = 0 AND sy.sdate <=  to_date('2019/04/22','YYYY/MM/DD')  AND sy.edate >=  to_date('2019/04/22','YYYY/MM/DD')  and ji.sdate <=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and ji.edate >=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and sz.sr_jig_cd = ji.sr_jig_cd and ji.sr_tkt_cd = 'DFLT'";
+    	System.out.println(qry11.replaceAll("SELECT", "select"));
+    	String a="123";
+    	String b=a;
+    	a="456";
+    	System.out.println(b);
+    	InetAddress address;
+		try {
+			address = InetAddress.getLocalHost();
+			System.out.println(address.getHostAddress());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+//String qry = "select e.emp_id,emp_no,emp_nm,sr_syz_nm from srw_syz_manager sm,srw_syozoku sz,emp e,srw_syainmst sy where  sm.sya_id = e.emp_id AND sm.sdate <=  to_date('2019/04/10','YYYY/MM/DD')  AND sm.edate >=  to_date('2019/04/10','YYYY/MM/DD')  AND sm.sr_syz_cd = sz.sr_syz_cd  AND sz.sdate <=  to_date('2019/04/10','YYYY/MM/DD')  AND sz.edate >=  to_date('2019/04/10','YYYY/MM/DD')  AND sz.status = 0  AND sm.sya_id = sy.sya_id AND sy.boss_kb = 0 AND sy.status = 0 AND sy.sdate <=  to_date('2019/04/10','YYYY/MM/DD')  AND sy.edate >=  to_date('2019/04/10','YYYY/MM/DD')  UNION SELECT e.emp_id,e.emp_no,e.emp_nm,sr_syz_nm  FROM srw_syz_submanager ss,srw_syozoku sz,emp e,srw_syainmst sy WHERE serviceName = 'root.cws.shuro.boss.time_input_approval' AND ss.sya_id = e.emp_id AND ss.sr_syz_cd = sz.sr_syz_cd  AND sz.sdate <=  to_date('2019/04/10','YYYY/MM/DD')  AND sz.edate >=  to_date('2019/04/10','YYYY/MM/DD')  AND ss.sdate <=  to_date('2019/04/10','YYYY/MM/DD')  AND ss.edate >=  to_date('2019/04/10','YYYY/MM/DD')  AND sz.status = 0  AND sy.sya_id = ss.sya_id AND sy.boss_kb = 0 AND sy.status = 0 AND sy.sdate <=  to_date('2019/04/10','YYYY/MM/DD')  AND sy.edate >=  to_date('2019/04/10','YYYY/MM/DD') ";
+	String qry="select em.sya_id,em.sya_bg,em.sya_nm,em.sya_kn,sz.sr_syz_nm from srw_syainmst sy,srw_syainjh em,srw_syozoku sz where  sy.sya_id = em.sya_id and sz.sr_syz_cd = sy.sr_syz_cd  and sy.sdate <=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and sy.edate >=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and sz.sdate <=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and sz.edate >=  to_date( to_char( sysdate, 'yyyy/mm/dd'), 'yyyy/mm/dd')  and sy.boss_kb = 0  and sy.status = 0  and sz.sr_syz_nm LIKE('%SilentValleyNationalPark%')  order by em.sya_bg asc ";	
+	qry = qry.toLowerCase();
+        int selectIndex = qry.indexOf("select");
+        while (selectIndex != -1) {
+        	System.out.println(qry.length());
+        	System.out.println(selectIndex);
+            String qry1=qry.substring(0,qry.indexOf(",",selectIndex));
+            String qry2=qry.substring(qry.indexOf("from",selectIndex)-1);
+            System.out.println(qry1);
+            System.out.println(qry2);
+            qry = qry1 + qry2;
+            selectIndex = qry.indexOf("select", selectIndex+1);
+        }
+        System.out.println(qry);
+		
+		
+		
+		
+		
+    	
     	SpringApplication.run(SampleApplication.class, args);
     }
     
